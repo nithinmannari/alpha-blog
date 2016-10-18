5f0edfe9b55c24ef1df4f474c7fbd9106556582d
nithinmannari <nithin.mannari@gmail.com>
2016-10-18 19:50:32 +0000

    update views with category

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
     
-    belongs_to :user_id
+    belongs_to :user
     has_many :article_categories
     has_many :categories, through: :article_categories
     validates :title, presence: true, length: {minimum: 3 , maximum:30} 
